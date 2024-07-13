d7999fa73209016004c0ebd6310623147c2c9c08
Ryo ikegata <ryo.ike.6153@gmail.com>
2024-07-13 17:28:12 +0900

    feat: :sparkles: テナント作成画面とログイン画面作成

--- src/app/(auth)/logIn/page.tsx
+++ src/app/(auth)/logIn/page.tsx
@@ -1,12 +1,65 @@
-import Copyright from "@/components/atoms/page";
-import Image from "next/image";
+'use client';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { supabase } from '@/utils/supabase/supabase';
 
 export default function LogIn() {
     return (
         <main>
-            <section className="w-300 m-auto">
-                <h1 className="text-xl font-bold">ログイン</h1>
-                <h2 className="text-lg font-bold">Googleログイン</h2>
+            <section className="w-300 m-auto flex flex-col h-screen justify-center">
+                <Auth
+                    supabaseClient={supabase}
+                    providers={['google']}
+                    localization={{
+                        variables: {
+                            sign_in: {
+                                email_label: 'メールアドレス',
+                                email_input_placeholder: 'メールアドレスを入力してください',
+                                password_label: 'パスワード',
+                                password_input_placeholder: 'パスワードを入力してください',
+                                button_label: 'ログイン',
+                                loading_button_label: 'ログイン中...',
+                                social_provider_text: 'Googleでログイン',
+                                link_text: '既にアカウントをお持ちですか？',
+                            },
+                            sign_up: {
+                                link_text: 'サインアップ',
+                                confirmation_text: 'アカウントをお持ちでない方はこちら',
+                                email_label: 'メールアドレス',
+                                email_input_placeholder: 'メールアドレスを入力してください',
+                                password_label: 'パスワード',
+                                password_input_placeholder: 'パスワードを入力してください',
+                                button_label: 'ログイン',
+                                loading_button_label: 'ログイン中...',
+                                social_provider_text: 'Googleでログイン',
+                            },
+                            magic_link: {
+                                link_text: 'メールでログイン',
+                                confirmation_text: 'メールでログイン',
+                                email_input_placeholder: 'メールアドレスを入力してください',
+                                button_label: 'ログイン',
+
+                            },
+                            forgotten_password: {
+                                link_text: 'パスワードをお忘れですか？',
+                                confirmation_text: 'パスワードをお忘れですか？',
+                                email_label: 'メールアドレス',
+                                email_input_placeholder: 'メールアドレスを入力してください',
+                                button_label: 'メールを送信',
+                                loading_button_label: '送信中...',
+                            },
+                        }
+                    }}
+                    appearance={{
+                        theme: ThemeSupa,
+                        style: {
+
+                        },
+                        className: {
+                            "button": "px-3 py-2 rounded-md font-medium bg-[#0584c7]",
+                        }
+                    }}
+                />
             </section>
         </main>
     );
