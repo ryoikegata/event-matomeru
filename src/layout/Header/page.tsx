e9a540735803c69a94cd540f27fdb7f58839339c
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-07-13 22:18:05 +0900

    add: ヘッダーのアイコンクリックでホームボタンへ戻る

--- src/layout/Header/page.tsx
+++ src/layout/Header/page.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 export const Header: FC = () => {
   return (
     <header className="fixed top-0 w-full shadow-md px-6 py-4 flex items-center justify-between bg-white">
-      <Image src="/logo.svg" alt="Logo" width={140} height={50} />
+      <Link href={'/'}>
+        <Image src="/logo.svg" alt="Logo" width={140} height={50} />
+      </Link>
       <div className="flex items-center gap-4">
         <Link href={'tenant-owner'}>
           <button className="flex flex-col items-center gap-1 cursor-pointer">
