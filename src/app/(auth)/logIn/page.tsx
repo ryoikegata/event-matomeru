05e08b7fb8f5eac7df0f9959d127dfc5bee06536
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-07-13 14:37:40 +0900

    add: ログイン画面最低限のUI作成

---
+++ src/app/(auth)/logIn/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function LogIn() {
+    return (
+        <main>
+            <section className="w-300 m-auto">
+                <h1 className="text-xl font-bold">ログイン</h1>
+                <h2 className="text-lg font-bold">Googleログイン</h2>
+            </section>
+        </main>
+    );
+}
