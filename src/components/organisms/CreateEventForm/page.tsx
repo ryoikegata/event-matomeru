c9d2bc41a79b3ad05864b4438bf69f7238f7bfec
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-07-14 02:39:47 +0900

    add: zodとreact-hook-formの配置は完了

--- src/components/organisms/CreateEventForm/page.tsx
+++ src/components/organisms/CreateEventForm/page.tsx
@@ -1,8 +1,30 @@
 import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EventSchema } from "@/services/schema";
+import { EventType } from "@/services/schema/types";
 
-export const CreateEventForm: FC = () => {
+export const CreateEventForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EventType>({
+    resolver: zodResolver(EventSchema),
+  });
+
+  const onSubmit = (data: EventType) => {
+    console.log(data);
+  };
+
+  // return {
+  //   register,
+  //   onSubmit: handleSubmit(onSubmit),
+  //   errors,
+  // };
+
   return (
-    <form action="" className="bg-white px-3 pt-4 pb-6">
+    <form action="" className="bg-white px-3 pt-4 pb-6" onSubmit={handleSubmit(onSubmit)}>
       <h1 className="flex pt-6 text-xl font-bold">イベント作成</h1>
       <div className="flex flex-col items-center gap-4 pt-4">
         <div className="w-full">
@@ -13,7 +35,9 @@ export const CreateEventForm: FC = () => {
             type="text"
             placeholder="イベント名を入力してください"
             className="border px-3 py-2 mt-1 w-full rounded"
+            {...register('title', { required: '入力が必須の項目です。' })}
           />
+          <p>{errors.title?.message}</p>
         </div>
         <div className="w-full">
           <label className="font-bold text-sm">
@@ -23,7 +47,9 @@ export const CreateEventForm: FC = () => {
             type="datetime-local"
             placeholder="イベント名を入力してください"
             className="border px-3 py-2 mt-1 rounded w-full"
+            {...register('start_at', { required: '入力が必須の項目です。',  })}
           />
+          <p>{errors.start_at?.message}</p>
         </div>
         <div className="w-full">
           <label className="font-bold text-sm">
@@ -33,19 +59,22 @@ export const CreateEventForm: FC = () => {
             type="datetime-local"
             placeholder="イベント名を入力してください"
             className="border px-3 py-2 mt-1 rounded w-full"
+            {...register('end_at', { required: '入力が必須の項目です。' })}
           />
+          <p>{errors.end_at?.message}</p>
         </div>
         <div className="w-full">
           <label className="font-bold text-sm">
             イベント詳細<span className="text-red-500"> *</span>
           </label>
           <textarea
-            name=""
             id=""
             placeholder="イベントの詳細を入力してください"
             className="border px-3 py-2 mt-1 rounded w-full resize-none"
             rows={7}
+            {...register('description', { required: '入力が必須の項目です。' })}
           ></textarea>
+          <p>{errors.description?.message}</p>
         </div>
         <button className="text-white bg-[#0584c7] rounded-full w-72 text-lg font-bold py-2">
           作成
--- src/services/schema/index.ts
+++ src/services/schema/index.ts
@@ -12,3 +12,12 @@ export const UserSchema = z.object({
   tenant_id: z.number(),
   role_id: z.number(),
 });
+
+export const EventSchema = z.object({
+  title: z.string().max(20, { message: "イベント名は20文字以内に収めてください" }),
+  start_at: z.string(),
+  end_at: z.string(),
+  // start_at: z.string().datetime({ offset: true }),
+  // end_at: z.string().datetime({ offset: true }),
+  description: z.string().max(100, { message: "詳細は100文字以内に収めてください" }),
+});
