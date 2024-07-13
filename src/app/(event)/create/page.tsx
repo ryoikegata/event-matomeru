02210f6db08110cf225ba6423eb33dcf8131e4d1
Gaku1031 <gaku.1031.basket@gmail.com>
2024-07-13 18:33:05 +0900

    fix: :bug: イベント作成モーダルを作成

--- src/app/(event)/create/page.tsx
+++ src/app/(event)/create/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { Header } from "@/layout/Header/page";
-import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
-import { EventBlock } from "@/components/atoms/EventBlock/page";
 
-export default function Home() {
-  const [select, setSelect] = useState<string>("all");
+export default function CreateEvent() {
   return (
     <>
       <Header />
@@ -15,22 +12,48 @@ export default function Home() {
         <h1 className="flex pt-6 text-3xl font-bold">イベント作成</h1>
         <div className="flex flex-col items-center gap-4 pt-4">
           <div className="">
-            <h2 className="font-bold">イベント名<span className="text-red-500"> *</span></h2>
-            <input type="text" placeholder="イベント名を入力してください" className="border p-1 rounded w-300"/>
+            <h2 className="font-bold">
+              イベント名<span className="text-red-500"> *</span>
+            </h2>
+            <input
+              type="text"
+              placeholder="イベント名を入力してください"
+              className="border p-1 rounded w-300"
+            />
           </div>
           <div className="">
-            <h2 className="font-bold">開始日時<span className="text-red-500"> *</span></h2>
-            <input type="datetime-local" placeholder="イベント名を入力してください" className="border p-1 rounded w-300"/>
+            <h2 className="font-bold">
+              開始日時<span className="text-red-500"> *</span>
+            </h2>
+            <input
+              type="datetime-local"
+              placeholder="イベント名を入力してください"
+              className="border p-1 rounded w-300"
+            />
           </div>
           <div className="">
-            <h2 className="font-bold">終了日時<span className="text-red-500"> *</span></h2>
-            <input type="datetime-local" placeholder="イベント名を入力してください" className="border p-1 rounded w-300"/>
+            <h2 className="font-bold">
+              終了日時<span className="text-red-500"> *</span>
+            </h2>
+            <input
+              type="datetime-local"
+              placeholder="イベント名を入力してください"
+              className="border p-1 rounded w-300"
+            />
           </div>
           <div className="">
             <h2 className="font-bold">イベント詳細</h2>
-            <textarea name="" id="" placeholder="イベントの詳細を入力してください" className="border p-1 rounded w-300 resize-none" rows={7}></textarea>
+            <textarea
+              name=""
+              id=""
+              placeholder="イベントの詳細を入力してください"
+              className="border p-1 rounded w-300 resize-none"
+              rows={7}
+            ></textarea>
           </div>
-          <button className="text-white bg-blue-500 rounded-full w-72 text-lg font-bold py-2">作成</button>
+          <button className="text-white bg-blue-500 rounded-full w-72 text-lg font-bold py-2">
+            作成
+          </button>
         </div>
       </main>
     </>
